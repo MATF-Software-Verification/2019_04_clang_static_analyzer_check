582b57597cb4b341bbcb44a02c1f3f926bb44495
Ognjen Plavsic <Ognjen.Plavsic@rt-rk.com>
2020-04-28 23:41:27 +0200

    Changed from type width to type precision. Checker now doesn't stop analysis after reporting warning.

--- ShiftingChecker.cpp
+++ ShiftingChecker.cpp
@@ -1,3 +1,10 @@
+//=- ShiftingChecker.cpp --------------------------*- C++ -*-==//
+// This checker implements rule 04.INT34-C by "SEI CERT C Coding Standard":
+// Do not shift an expression by a negative number of bits or by greater than or
+// equal to the number of bits that exist in the operand.
+//===----------------------------------------------------------------------===//
+//
+
 #include "Taint.h"
 #include "clang/StaticAnalyzer/Checkers/BuiltinCheckerRegistration.h"
 #include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
@@ -12,8 +19,7 @@ using namespace taint;
 namespace {
 class ShiftingChecker : public Checker<check::PreStmt<BinaryOperator>> {
   mutable std::unique_ptr<BuiltinBug> BT;
-  void reportBug(const std::string &Msg, ProgramStateRef StateZero,
-                 CheckerContext &C,
+  void reportBug(const std::string &Msg, CheckerContext &C,
                  std::unique_ptr<BugReporterVisitor> Visitor = nullptr) const;
 
 public:
@@ -29,9 +35,9 @@ static const Expr *getRHSExpr(const ExplodedNode *N) {
 }
 
 void ShiftingChecker::reportBug(
-    const std::string &Msg, ProgramStateRef StateFalse, CheckerContext &C,
+    const std::string &Msg, CheckerContext &C,
     std::unique_ptr<BugReporterVisitor> Visitor) const {
-  if (ExplodedNode *N = C.generateErrorNode(StateFalse)) {
+  if (ExplodedNode *N = C.generateNonFatalErrorNode()) {
     if (!BT)
       BT.reset(new BuiltinBug(this, "Invalid shift operation"));
 
@@ -82,15 +88,26 @@ void ShiftingChecker::checkPreStmt(const BinaryOperator *B,
   if (TypeOfLHS.isNull())
     return;
 
-  uint64_t BitWidth = ACtx.getTypeInfo(TypeOfLHS.getTypePtr()).Width;
+  // Second paragraph in this rule rationale states following:
+  // "The precision of an integer type is the number of bits it uses to
+  // represent values, excluding any sign and padding bits. For unsigned integer
+  // types, the width and the precision are the same; whereas for signed integer
+  // types, the width is one greater than the precision."
+  uint64_t TypePrecision =
+      TypeOfLHS->isSignedIntegerType()
+          ? ACtx.getTypeInfo(TypeOfLHS.getTypePtr()).Width + 1
+          : ACtx.getTypeInfo(TypeOfLHS.getTypePtr()).Width;
 
   // Make symbolic integer value that represents maximum value which can be used
   // in shifting of the type of the left hand side.
-  llvm::APInt LLVMIntegerBitWidth = llvm::APInt(32, BitWidth);
+  llvm::APInt TypePrecisionAsLLVMInteger = llvm::APInt(
+      ACtx.getTypeInfo(TypeOfRHS.getTypePtr()).Width, TypePrecision);
+
   SValBuilder &SVB = C.getSValBuilder();
-  nonloc::ConcreteInt MaxShiftingSVal = SVB.makeIntVal(
-      clang::IntegerLiteral::Create(C.getASTContext(), LLVMIntegerBitWidth,
-                                    B->getRHS()->getType(), SourceLocation()));
+  nonloc::ConcreteInt MaxShiftingSVal =
+      SVB.makeIntVal(clang::IntegerLiteral::Create(
+          C.getASTContext(), TypePrecisionAsLLVMInteger, B->getRHS()->getType(),
+          SourceLocation()));
 
   SVal InvalidShiftOperation =
       SVB.evalBinOp(C.getState(), BO_GE, RhsSVal, MaxShiftingSVal, ACtx.BoolTy);
@@ -113,15 +130,17 @@ void ShiftingChecker::checkPreStmt(const BinaryOperator *B,
     reportBug("Invalid shift operation. Type '" +
                   TypeOfLHS.getUnqualifiedType().getAsString() +
                   "' can't be shifted by more than '" +
-                  std::to_string(BitWidth - 1) + "' bits",
-              StateFalse, C);
+                  std::to_string(TypePrecision - 1) + "' bits",
+              C);
     return;
   }
 
   bool TaintedD = isTainted(C.getState(), *DV);
-  if ((StateFalse && StateTrue && TaintedD)) {
-    reportBug("Shifted by a tainted value", StateFalse, C,
-              std::make_unique<taint::TaintBugVisitor>(*DV));
+  if (StateFalse && StateTrue) {
+    (TaintedD == true)
+        ? reportBug("Shifing with a tainted value", C,
+                    std::make_unique<taint::TaintBugVisitor>(*DV))
+        : reportBug("Shifting with an undefined value", C);
     return;
   }
 }
